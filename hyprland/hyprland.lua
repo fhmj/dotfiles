258190c8959899ee1765cda5ebf8356779cb4e5c
Frank Jensen <frankhjorth@gmail.com>
2026-08-15 21:41:32 +0200

    Install quickshell

--- hyprland/hyprland.lua
+++ hyprland/hyprland.lua
@@ -32,11 +32,9 @@ local workBrowser = "firefox -p Work --name firefox-work"
 -- Autostart necessary processes (like notifications daemons, status bars, etc.)
 -- Or execute your favorite apps at launch like this:
 --
--- hl.on("hyprland.start", function ()
---   hl.exec_cmd(terminal)
---   hl.exec_cmd("nm-applet")
---   hl.exec_cmd("waybar & hyprpaper & firefox")
--- end)
+hl.on("hyprland.start", function ()
+  hl.exec_cmd("quickshell")
+end)
 
 
 -------------------------------
